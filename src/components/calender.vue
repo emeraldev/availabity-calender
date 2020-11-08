3c16b09d1a362889ce78034a20794d14eaf1006e
kabaso <sonny@envisionit.africa>
2020-11-08 21:48:00 +0200

    init commit

---
+++ src/components/calender.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+// import dates from '../data/dates.json'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    tuples: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      items: [],
+      fields: this.tuples
+    }
+  },
+  mounted() {
+    console.log(this.data)
+  },
+  watch: {
+    data(val) {
+      console.log(val)
+      this.items = val
+    }
+  }
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <b-container>
+    <b-card title="AVAILABILITY CALENDAR " sub-title="Bluegrass Digital Limited">
+      <div>
+        <label for="range-1">{{ jobLength }} HR/s</label>
+        <b-form-input id="range-1" v-model="jobLength" @change="setLength" type="range" min="0" max="7"></b-form-input>
+      </div>
+      <calender :data="table" :tuples="fields"/>
+    </b-card>
+  </b-container>
+</template>
+<script>
+import Calender from './calender'
+import dates from '../data/dates.json'
+
+export default {
+  components: {
+    Calender
+  },
+  data: () => ({
+    full: [9,10,11,12,13,14,15,16,17],
+    fields: [],
+    job: null,
+    table: [],
+    jobLength: 0,
+  }),
+  methods: {
+    checkSlotAvailability(time, jobLength, date, availabilty) {
+      this.job = availabilty.find(slot => slot === time)
+      if (this.job) {
+        var index = this.full.indexOf(time)
+        var fullTime = (index + jobLength) - 1
+        var nxt = this.full[fullTime + 1]
+        var buffer = availabilty.find(slot => slot === nxt)
+        console.log(jobLength)
+        if (buffer) {
+          return 'AVAILABLE'
+        } else {
+          return 'UNAVAILABLE'
+        }
+      } else {
+        return 'FULL'
+      }
+    },
+    setLength() {
+      this.table = []
+      this.fields.push('___')
+      var count1 = 0
+      this.full.forEach(time => { 
+      var count = 0
+        dates.forEach(date => {
+          this.fields.push(date.Date)
+          let slot = this.checkSlotAvailability(time, this.jobLength, date.Date, date.HoursAvailable)
+          var item = []
+          /* item[count1] = {
+            '___': count, '2016-05-18': date.Date === '2016-05-18' ? slot : '',
+            '2016-05-19': date.Date === '2016-05-19' ? slot : '',
+            '2016-05-20': date.Date === '2016-05-20' ? slot : '',
+            '2016-05-21': date.Date === '2016-05-21' ? slot : '', 
+            '2016-05-23': date.Date === '2016-05-23' ? slot : '',
+            '2016-05-24': date.Date === '2016-05-24' ? slot : '',
+          } */
+
+          item[count1] = {
+            '___': count, 
+            '2016-05-18': '',
+            '2016-05-19': '',
+            '2016-05-20': '',
+            '2016-05-21': '', 
+            '2016-05-23': '',
+            '2016-05-24': '',
+          }
+
+          if (date.Date === '2016-05-18') {
+            item[count1]['2016-05-18'] = slot
+          } 
+          if (date.Date === '2016-05-19') {
+            item[count1]['2016-05-19'] = slot
+          } 
+          if (date.Date === '2016-05-20') {
+            item[count1]['2016-05-20'] = slot
+          } 
+          if (date.Date === '2016-05-21') {
+            item[count1]['2016-05-21'] = slot
+          } 
+          if (date.Date === '2016-05-23') {
+            item[count1]['2016-05-23'] = slot
+          }
+          if (date.Date === '2016-05-24') {
+            item[count1]['2016-05-24'] = slot
+          }
+
+          
+          //console.log(count)
+
+          this.table.push(item[count1])
+          date.HoursAvailable.forEach(avb => {
+            if (time === avb) {
+              // console.log(avb)
+            }
+            
+          });
+          count++
+        });
+        count1++
+      });
+    }
+  },
+  mounted() {
+    this.setLength()
+  }
+};
+</script>
